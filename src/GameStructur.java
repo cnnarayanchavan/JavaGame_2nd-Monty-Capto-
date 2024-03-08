926c7def70f1e5ee624dfc71fbec3335bc104f6e
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-08 11:48:05 +0530

    Update GameStructur.java_added the button array which is clickable from line 50-54

--- src/GameStructur.java
+++ src/GameStructur.java
@@ -11,12 +11,14 @@ public class GameStructur {
     JFrame frame=new JFrame("MAARIIYOO");      //for game window modification.
     JLabel textLabel=new JLabel();             //for title of game in big font.
     JPanel textPanel=new JPanel();             //Panel to hold the lable.
-    JPanel boardPanel=new JPanel();
+    JPanel boardPanel=new JPanel();            //for the boardtext or space
+    JButton[] board = new JButton();
     
 
     GameStructur(){
 
-        //defined the windo dimentions.
+        //defined the windo dimentions.-------------------------------------------------------
+        
         frame.setVisible(true);
         frame.setSize(WinWidth,WinHeight);
         frame.setLocationRelativeTo(null);
@@ -24,21 +26,34 @@ public class GameStructur {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        //for text (Title of game)
+        //for text (Title of game)-----------------------------------------------------------
+        
         textLabel.setFont(new Font ("Ariel", Font.PLAIN, 50));
         textLabel.setHorizontalAlignment(JLabel.CENTER);          // for aligining the text into the center os window 
         textLabel.setText("Score : 0");
         textLabel.setOpaque(true);
 
-        //adding textlabel into the text panel to show up on screem.
+        //adding textlabel into the text panel to show up on screem.-----------------------------------------------
+        
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLabel);                               //Seted the text to the upper north position.
         frame.add(textPanel, BorderLayout.NORTH);               //frame.add(<>)  this is for addinf the updates into the frame       
         
-        //this is for board panel 
+        //this is for board panel -----------------------------------------------------------------
+        
         boardPanel.setLayout(new GridLayout(5,5));
         boardPanel.setBackground(Color.pink);               //the background color is optional 
         frame.add(boardPanel);
+
+        //For adding button (clickable) used JButton from JFrame and added them into the frame------------------------------
+
+        for(i=0;i<9;i++){
+            JButton tile = new JButton();                //using for loop for buttons 
+            board[i]=tile;
+            boardPanel.add(title);
+        }
+
+        
 
   }
 }
